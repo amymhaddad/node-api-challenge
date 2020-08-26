cb4930463f008e49460fbabf1053ce123e3537fb
Amy M Haddad <amy.m.haddad@gmail.com>
2020-08-26 07:22:03 -0400

    Added endpoints for get and put reqs

--- data/helpers/projectModel.js
+++ data/helpers/projectModel.js
@@ -11,24 +11,33 @@ module.exports = {
 
 function get(id) {
   let query = db("projects as p");
-
+  //If there's an id, get the first matching id
   if (id) {
     query.where("p.id", id).first();
 
+    //Create a promise to query the projects based on id
     const promises = [query, getProjectActions(id)]; // [ projects, actions ]
 
+    //Return the results of the promise, which contains a project and actions
     return Promise.all(promises).then(function(results) {
+      //Destructure the contents of the promise
       let [project, actions] = results;
 
+      //IF there's a project (from the result of the project)
       if (project) {
+        //Get the actions (from the promise) associated with the project
         project.actions = actions;
 
+        //Return the project body 
         return mappers.projectToBody(project);
       } else {
         return null;
       }
     });
+  
   } else {
+    //IF there's NO id, then return the results of the query.
+    //This returns the projects 
     return query.then(projects => {
       return projects.map(project => mappers.projectToBody(project));
     });
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -4,14 +4,64 @@ router = express.Router()
 
 const Project = require("../data/helpers/projectModel")
 
-//Model is the class that you access the DB 
+//Model is the class that gives you access the DB 
 
 router.get("/", (req, res) => {
     Project.get()
     .then(projects => {
         return res.status(200).json(projects)
-        // console.log("project", projects)
+    })
+    .catch(error => {
+        return res.status(500).json({ error: "Server Error" })
+    })
+})
 
+//The result of this req includes "actions" for each project
+//Not sure why bc "actions" is not a column in the table. BUT it seems to be getting applied from the mappers function
+router.get("/:projectId", (req, res) => {
+    const projectId = req.params.projectId
+
+    if (isNaN(projectId)) {
+        return res.status(400).json({ message: "Invalid syntax"})
+    }
+    Project.get(projectId)
+    .then(project => {
+        return res.status(200).json(project)
+    })
+    .catch(error => {
+        console.log("err", error)
+        return res.status(500).json({ error: "Server Error" })
+    })
+})
+
+router.put("/:projectId", (req, res) => {
+    const projectId = req.params.projectId
+    const name = req.body.name
+    const description = req.body.description
+    const changes = {name, description}
+
+    if (isNaN(projectId)) {
+        return res.status(400).json({ message: "Invalid syntax"})
+    }
+
+    if (!name || !description) {
+        return res.status(400).json({ message: "The name and description are required fields."})
+    }
+
+    Project.update(projectId, changes)
+    .then(updatedProject => {
+        if (!updatedProject) {
+            return res.status(404).json({ error: "Project id is not found"})
+        }
+        //Should I pull out the name and description to send back?
+        const updatedName = updatedProject.name
+        const udpatedDescription = updatedProject.description
+        const allUpdates = {updatedName, udpatedDescription}
+        return res.status(201).json(allUpdates) 
+    })
+    .catch(err => {
+        console.log("err", err)
+        return res.status(500).json({ error: "Server Error" })
     })
 })
 
